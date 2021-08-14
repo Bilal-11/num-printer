5c8d1e4516f9103a0a2989ff0181c327845ca5f7
Bilal-11 <sbilalahmad2000@gmail.com>
2021-08-14 11:35:28 +0530

    Added code to print 1 in large-# format

--- num-printer/num-printer.cpp
+++ num-printer/num-printer.cpp
@@ -3,13 +3,22 @@
 #include<string>
 using namespace std;
 
+const char* one[] = { " #   ", "##   "," #   "," #   ", " #   ", " #   ", " #   ", "###  " };
+
 int main()
 {
+	// Accept 5-digit number as a string
 	char num[6];
 	cout << "Enter a 5-digit number: ";
 	cin >> num;
 
-	cout << endl << "The number you entered is: " << num;
+	cout << endl << "The number you entered is: " << num << endl << endl;
+	
+	for (int i = 0; i < 8; i++)
+	{
+		cout << one[i] << endl;
+	}
+	
 
 	return 0;
 }
