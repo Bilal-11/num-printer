34c5aa318f37fa74c366ceb089d460393a65d3ce
Bilal-11 <sbilalahmad2000@gmail.com>
2021-08-14 09:25:11 +0530

    Added code that can accept a five digit number

---
+++ num-printer/num-printer.cpp
@@ -0,0 +1,15 @@
+// Program to accept a 5-digit number and print it in large-# format
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	char num[6];
+	cout << "Enter a 5-digit number: ";
+	cin >> num;
+
+	cout << endl << "The number you entered is: " << num;
+
+	return 0;
+}
