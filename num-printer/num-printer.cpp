43794aa4e129a362cf8849dba22cfd61ef34c129
Bilal-11 <sbilalahmad2000@gmail.com>
2021-08-14 15:50:42 +0530

    Revert "Created function 'painter' that prints the number in large-# format"
    This reverts commit 3f34879e0747eea7afaa6eb4dbb063943d5a7855.

--- num-printer/num-printer.cpp
+++ num-printer/num-printer.cpp
@@ -3,8 +3,6 @@
 #include<string>
 using namespace std;
 
-void painter(char*);
-
 // Array of pointers for each digit that points to all parts of the large-# format of that digit
 const char* one[] = { " #   ", "##   "," #   "," #   ", " #   ", " #   ", " #   ", "###  " };
 const char* two[] = {"#####  ","    #  ","    #  " ,"    #  " ,"#####  ","#      ","#      ","#####  " };
@@ -17,9 +15,6 @@ const char* eight[] = {" ####   ","#    #  ","#    #  " ," ####   " ,"#    #  " 
 const char* nine[] = { "#####   ","#    #  ","#    #  " ," ####   " ,"     #  " ,"     #  " ,"     #  " ," ####   " };
 const char* zero[] = { " ####   ","#    #  ","#    #  " ,"#    #  " ,"#    #  " ,"#    #  " ,"#    #  " ," ####   " };
 
-// Array of pointers to the array of pointers to each digit's large-# data
-const char** digits[] = {zero,one,two,three,four,five,six,seven,eight,nine};
-
 int main()
 {
 	// Accept 5-digit number as a string
@@ -27,61 +22,13 @@ int main()
 	cout << "Enter a 5-digit number: ";
 	cin >> num;
 
-	cout << endl;
+	cout << endl << "The number you entered is: " << num << endl << endl;
 	
-	// Print the number in large-# format using the function painter
-	painter(num);
-		
+	for (int i = 0; i < 8; i++)
+	{
+		cout << one[i] << two[i] << three[i] << four[i] << five[i] << six[i] << seven[i] << eight[i] << nine[i] << zero[i] << endl;
+	}
+	
+
 	return 0;
-}
-
-void painter(char* n)
-{
-	int index[5];
-
-	// Store each digit of entered number in array index (in order)
-	for (int i = 0; i < 5; i++)
-	{
-		switch (n[i])
-		{
-		case '0':
-			index[i] = 0;
-			break;
-		case '1':
-			index[i] = 1;
-			break;
-		case '2':
-			index[i] = 2;
-			break;
-		case '3':
-			index[i] = 3;
-			break;
-		case '4':
-			index[i] = 4;
-			break;
-		case '5':
-			index[i] = 5;
-			break;
-		case '6':
-			index[i] = 6;
-			break;
-		case '7':
-			index[i] = 7;
-			break;
-		case '8':
-			index[i] = 8;
-			break;
-		case '9':
-			index[i] = 9;
-			break;
-		}
-	}
-
-	// Print the large-# format of the number.
-	// The array index is used to go to a particular digit's large-# data and j cycles through
-	// that data.
-	for (int j = 0; j < 8; j++)
-	{
-		cout << digits[index[0]][j] << digits[index[1]][j] << digits[index[2]][j] << digits[index[3]][j] << digits[index[4]][j] << endl;
-	}
 }
